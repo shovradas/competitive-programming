7edec977ccf8c5c5c4e3d3d14aca88c1d775b952
Shovra <shovradas@gmail.com>
2020-08-14 20:26:50 +0200

    hackerrank code added

---
+++ hackerrank/README.md
@@ -0,0 +1,2 @@
+### Python
+- `find-a-string.py` [problem-link](https://www.hackerrank.com/challenges/find-a-string/problem)
---
+++ hackerrank/python/find-a-string.py
@@ -0,0 +1,16 @@
+# Problem Link: https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    count = 0
+    len_sub = len(sub_string)
+    for i in range(len(string)-len_sub+1):
+        if string[i:i+len_sub] == sub_string:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
