d72f3a77fe029ace44ff83351bc5ebcd306982a8
Shovra <shovradas@gmail.com>
2020-10-15 19:04:03 +0200

    regular update

--- hackerrank/README.md
+++ hackerrank/README.md
@@ -61,6 +61,7 @@
 - `words-score` [problem-link](https://www.hackerrank.com/challenges/words-score/problem)
 - `validate-list-of-email-address-with-filter` [problem-link](https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem)
 - `html-parser-part-1` [problem-link](https://www.hackerrank.com/challenges/html-parser-part-1/problem)
+- `html-parser-part-2` [problem-link](https://www.hackerrank.com/challenges/html-parser-part-2/problem)
 
 
 ### C++
---
+++ hackerrank/python/html-parser-part-2.py
@@ -0,0 +1,23 @@
+# Problem Link: https://www.hackerrank.com/challenges/html-parser-part-2/problem
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if data.strip():
+            comment_type = 'Multi' if '\n' in data else 'Single'
+            print(f'>>> {comment_type}-line Comment', data, sep='\n')
+  
+    def handle_data(self, data):
+        if data.strip():
+            print('>>> Data', data, sep='\n')
+
+
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
