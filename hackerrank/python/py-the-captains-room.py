eac1ab07bfce0e332b81b846bc8e9e2a1d88bfa5
Shovra <shovradas@gmail.com>
2020-11-01 19:55:36 +0100

    regular update

---
+++ hackerrank/python/py-the-captains-room.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/py-the-captains-room/problem
+
+k, allocation = int(input()), input().split()
+
+rooms, group_rooms = set(), set()
+for r in allocation:
+    if r in rooms:
+        group_rooms.add(r)
+    else:
+        rooms.add(r)
+
+captains_room = rooms - group_rooms
+print(captains_room.pop())
